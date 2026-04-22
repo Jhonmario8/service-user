5aff4162242d9dc02ab9e12cec01ceff8a6aa21b
Jhonma <jhonma1238@gmail.com>
2026-04-21 22:20:52 -0500

    Add validation annotations for email and password fields in AuthDTO

--- src/main/java/com/pragma/plazoleta/application/dto/AuthDTO.java
+++ src/main/java/com/pragma/plazoleta/application/dto/AuthDTO.java
@@ -2,6 +2,8 @@ package com.pragma.plazoleta.application.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.pragma.plazoleta.application.constants.ApplicationConstants;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -11,7 +13,9 @@ import lombok.*;
 @ToString
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class AuthDTO {
+    @NotBlank(message = ApplicationConstants.EMAIL_CANNOT_BE_BLANK)
     private String email;
+    @NotBlank(message = ApplicationConstants.PASSWORD_CANNOT_BE_BLANK)
     private String password;
     private String token;
 }
